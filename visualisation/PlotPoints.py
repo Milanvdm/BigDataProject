30da84c45c976204a60c2e972b272582e21942db
Milan van der Meer <milanvdm@hotmail.com>
2015-04-12 18:18:19 +0800

    finished visualisation

--- visualisation/PlotPoints.py
+++ visualisation/PlotPoints.py
@@ -1,25 +1,40 @@
 __author__ = 'Milan'
 
 import folium
-import pandas as pd
-
-from CreateDatafilesFromPoints import CreateData
-
-createData = CreateData()
-createData.createData()
-createData.csvFile.close()
-
-allDatapoints = r'points.json'
-ratings = r'ratings.csv'
-
-ratings_data = pd.read_csv(ratings, skip_blank_lines=True)
+import CreateDataPoints as cd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 #Let Folium determine the scale
 map = folium.Map(location=[48, -102], zoom_start=3)
-map.geo_json(geo_path=allDatapoints, data=ratings_data,
-             columns=['IdNumber', 'Rating'],
-             key_on='feature.idNumber',
-             fill_color='YlGn', fill_opacity=0.7, line_opacity=0.2,
-             legend_name='Ratings')
+cmap = plt.get_cmap("afmhot")
+mixer = mpl.cm.ScalarMappable(mpl.colors.Normalize(vmin=0, vmax=5), cmap)
+
+def makeMarkers():
+    x, y, z = createData()
+
+    i = 0
+    while i < x.size:
+        color = decideColor(z[i])
+        map.polygon_marker(location=[x[i], y[i]],
+                     fill_color=color, line_opacity=0, num_sides=4, radius=5, rotation=0)
+
+        i += 1
+
+def decideColor(ratingValue):
+    hexcolor = mpl.colors.rgb2hex(mixer.to_rgba(ratingValue))
+    print str(hexcolor)
+    return str(hexcolor)
+
+def createData():
+        centroid = [48, -102, 3]
+        variance = [1, 1, 2]
+        x, y, z = cd.makeDataPointsFromCluster(centroid, variance, 1000)
+
+        return x, y, z
+
+
+
+makeMarkers()
 map.create_map(path='clusterMap.html')
 
