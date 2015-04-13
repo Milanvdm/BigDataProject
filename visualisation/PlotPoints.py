99f53dbe57608c1fc6e6ff7671b2cab5d1a03e1b
Milan van der Meer <milanvdm@hotmail.com>
2015-04-13 15:50:45 +0800

    Made parser for clusterData

--- visualisation/PlotPoints.py
+++ visualisation/PlotPoints.py
@@ -5,16 +5,19 @@ import CreateDataPoints as cd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
+import ast
+
+from BigDataProject.bfr.Cluster import Cluster
+
 #Let Folium determine the scale
 map = folium.Map(location=[48, -102], zoom_start=3)
 cmap = plt.get_cmap("afmhot")
 mixer = mpl.cm.ScalarMappable(mpl.colors.Normalize(vmin=0, vmax=5), cmap)
 
-def makeMarkers():
-    x, y, z = createData()
+def makeMarkers(x, y, z):
 
     i = 0
-    while i < x.size:
+    while i < len(x):
         color = decideColor(z[i])
         map.polygon_marker(location=[x[i], y[i]],
                      fill_color=color, line_opacity=0, num_sides=4, radius=5, rotation=0)
@@ -23,18 +26,38 @@ def makeMarkers():
 
 def decideColor(ratingValue):
     hexcolor = mpl.colors.rgb2hex(mixer.to_rgba(ratingValue))
-    print str(hexcolor)
     return str(hexcolor)
 
-def createData():
-        centroid = [48, -102, 3]
-        variance = [1, 1, 2]
-        x, y, z = cd.makeDataPointsFromCluster(centroid, variance, 1000)
+def createMap():
+        clusters = readClusters()
 
-        return x, y, z
+        for cluster in clusters:
+            centroid = cluster.getCentroid()
+            centroid[2] = centroid[2]/10
+            variance = cluster.getVariance()
+            variance[2] = variance[2]/10
+            amount = cluster.N
 
+            x, y, z = cd.makeDataPointsFromCluster(centroid, variance, 10)
 
+            makeMarkers(x, y, z)
 
-makeMarkers()
+def readClusters():
+    clusters = []
+
+    with open("clusters.txt", "r") as input:
+        for line in input:
+            lst = ast.literal_eval(line)
+
+            newCluster = Cluster()
+            newCluster.N = lst[0]
+            newCluster.SUM = lst[1]
+            newCluster.SUMSQ = lst[2]
+
+            clusters.append(newCluster)
+
+    return clusters
+
+createMap()
 map.create_map(path='clusterMap.html')
 
